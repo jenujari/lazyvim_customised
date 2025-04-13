f671cf366516ded8f9df9a7581fdd6619d452282
Jenish Jariwala <jhon.5456@gmail.com>
2025-04-13 17:10:44 +0000

    customized initial things

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map({ "n", "v" }, "0", "^", { desc = "beginning of line" })
+map({ "n", "v" }, "<leader><cr>", "noh<cr>", { desc = "Disable highlight when <leader><cr> is pressed" })
+
+
+vim.cmd([[
+  " indentation in visual mode
+  vnoremap > >gv
+  vnoremap < <gv
+
+  " buffer navigation
+  map bn :bnext<cr>
+  map bp :bprevious<cr>
+
+  " line movers  up: . | down ,
+  nnoremap <A-,> :m+<CR>==
+  nnoremap <A-.> :m-2<CR>==
+  inoremap <A-,> <Esc>:m+<CR>==gi
+  inoremap <A-.> <Esc>:m-2<CR>==gi
+  vnoremap <A-,> :m'>+<CR>gv=gv
+  vnoremap <A-.> :m-2<CR>gv=gv
+]])
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,43 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.cmd([[
+  set guifont=JetBrainsMono\ NF:h10
+  set ruler
+  set showmatch 
+  set renderoptions=type:directx,geom:1,renmode:5,taamode:1,level:0.5
+  set encoding=utf-8
+  set nocompatible
+  set tabstop=2 shiftwidth=2 softtabstop=2 expandtab 
+  set colorcolumn=120
+  set ignorecase smartcase
+  set hlsearch
+  set incsearch
+  set cursorline
+  set scrolloff=8
+  set belloff=all
+  set wildmenu
+  set hidden
+  set noswapfile
+  set nobackup
+  set nowritebackup
+  set autoread
+  set backspace=start,eol,indent
+  set mouse=a
+  set mousef
+  set scf
+  set mh
+  set wrap
+  set whichwrap+=b,s,<,>,[,],h,l
+  set foldmethod=indent
+  set nofoldenable
+  set path+=**
+  set lz
+  set autoindent          " copy indent from current line when starting a new line
+  set copyindent          " copy the previous indentation on autoindenting
+  set smartindent         " even better autoindent (e.g. add indent after '{')
+  set ut=2000
+  set history=500
+  set magic 
+]])
